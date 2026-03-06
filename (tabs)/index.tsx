bdeff53b37b9946731c2b79091c9a2a81f21ae17
arthursantosdealmeida168-ux <arthursantosdealmeida168@gmail.com>
2026-03-06 17:17:16 -0300

    v2026

---
+++ (tabs)/index.tsx
@@ -0,0 +1,16 @@
+import useTheme from "@/hooks/useTheme";
+import { createHomeStyles } from "@/styles/home.styles";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  const { toggleDarkMode, colors } = useTheme();
+  const homeStyles = createHomeStyles(colors)
+
+  return (
+    <SafeAreaView style={homeStyles.safeArea}>
+        <Text >HOME</Text>
+        <TouchableOpacity onPress={toggleDarkMode}>MUDA TEMA</TouchableOpacity>
+    </SafeAreaView>
+  ) 
+}
