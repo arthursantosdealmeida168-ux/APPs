8b3b5be9668cf333370e8d4d379965228890ec0a
arthursantosdealmeida168-ux <arthursantosdealmeida168@gmail.com>
2026-03-06 18:04:03 -0300

    Userteste

---
+++ (tabs)/use.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const use = () => {
+  return (
+    <View>
+      <Text>use</Text>
+    </View>
+  )
+}
+
+export default use
---
+++ styles/use.styles.ts
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const use.styles = () => {
+  return (
+    <View>
+      <Text>use.styles</Text>
+    </View>
+  )
+}
+
+export default use.styles
